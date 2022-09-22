44c143c830e5ee877fb4ddfdb3280fe83fc44dc6
Ryan Adam <2746755+rfadam@users.noreply.github.com>
2022-09-22 22:59:56 +0100

    Ensure that only published posts appear on index page

--- src/templates/IndexTemplate/IndexTemplate.tsx
+++ src/templates/IndexTemplate/IndexTemplate.tsx
@@ -50,7 +50,7 @@ export const query = graphql`
       limit: $limit
       skip: $offset
       sort: { order: DESC, fields: [frontmatter___date] }
-      filter: { frontmatter: { template: { eq: "post" }, draft: { ne: true } } }
+      filter: { frontmatter: { template: { eq: "post" }, draft: { ne: true }, published: { ne: false } } }
     ) {
       edges {
         node {
