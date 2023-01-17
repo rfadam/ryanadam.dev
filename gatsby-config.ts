38ab98ecb2ff61384aa4db677a32d4f0c6a3e8e5
Ryan Adam <2746755+rfadam@users.noreply.github.com>
2023-01-17 23:32:31 +0000

    Add in packages previously used

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -199,5 +199,8 @@ export default {
     "gatsby-plugin-catch-links",
     "gatsby-plugin-optimize-svgs",
     "gatsby-plugin-sass",
+    `gatsby-plugin-netlify`,
+    `gatsby-plugin-twitter`,
+    `gatsby-plugin-instagram-embed`,
   ],
 };
